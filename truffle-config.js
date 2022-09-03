b7a7e1acdd648f2f111b61ca1ab10773e86e22be
Tuba Asif <68794712+tuba-theProgrammer@users.noreply.github.com>
2022-09-03 22:13:44 +0500

    basic structure commit

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,4 @@
-require('babel-register');
-require('babel-polyfill');
+
 
 module.exports = {
   networks: {
@@ -13,6 +12,7 @@ module.exports = {
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
+      version: ">=0.7.0 <0.9.0",
       optimizer: {
         enabled: true,
         runs: 200,
